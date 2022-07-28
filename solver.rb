cd67fd04033dfa748256c965c9605064e9bcc129
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-07-28 14:28:09 -0300

    Resolve positive factorials

--- solver.rb
+++ solver.rb
@@ -2,5 +2,10 @@ class Solver
   def factorial(n)
     raise('No negative allowed for factorial.') if n.negative?
     1 if n == 0
+    factorial = 1
+    (1..n).each do
+      |num| factorial *= num
+    end
+    factorial
   end
 end
