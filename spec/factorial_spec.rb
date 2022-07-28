bbdc87d350589e9f4a804da0a4366fd6a34c3a1b
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-07-28 14:17:10 -0300

    Add negative factorial test

--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -2,12 +2,16 @@ require '../solver.rb'
 
 describe Solver do
   context "factorial method test" do
+    before :each do 
+      @my_solver = Solver.new
+    end
     it "factorial of 0 is 1" do
-      my_solver = Solver.new
-      result = my_solver.factorial(0)
+      result = @my_solver.factorial(0)
       expect(result).to eq(1)
     end
-  
+    it "factorial of negative raises an Exception" do
+      expect{@my_solver.factorial(-1)}.to raise_error('No negative allowed for factorial.')
+    end
   end
 
 end
