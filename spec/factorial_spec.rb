8ae17a36c844767f6b5f5c59bfff7a07cbc61984
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-07-28 14:06:22 -0300

    Factorial first test

---
+++ spec/factorial_spec.rb
@@ -0,0 +1,11 @@
+describe Solver do
+  context "factorial method test" do
+    it "factorial of 0 is 1" do
+      my_solver = Solver.new
+      result = my_solver.factorial(0)
+      expect(result).to eq(1)
+    end
+  
+  end
+
+end
