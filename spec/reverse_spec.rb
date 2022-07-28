2d57ca6f28dc34f958f6c271ca12c31194e8bc90
alexpuente0 <99830261+alexpuente0@users.noreply.github.com>
2022-07-28 13:04:53 -0500

    Add tests with numbers, symbols and spaces

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -7,5 +7,30 @@ describe Solver do
       result = solver.reverse('')
       expect(result).to eq('')
     end
+
+    it 'returnes reversed word' do
+      solver = Solver.new
+      result = solver.reverse('mathematician')
+      expect(result).to eq('naicitamehtam')
+    end
+
+    it 'returnes reversed word' do
+      solver = Solver.new
+      result = solver.reverse('$2.50')
+      expect(result).to eq('05.2$')
+    end
+
+    it 'returnes reversed word' do
+      solver = Solver.new
+      result = solver.reverse('Hello World ')
+      expect(result).to eq(' dlroW olleH')
+    end
+
+    it 'returnes reversed word' do
+      solver = Solver.new
+      result = solver.reverse('¯\\(ツ)/¯ /n')
+      expect(result).to eq('n/ ¯/)ツ(\\¯')
+    end
+
   end
 end
