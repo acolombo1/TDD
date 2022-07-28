69ec2ae818f0cec5e0693b5444ae0120eea3ba42
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-07-28 16:50:42 -0300

    Resolve remaining cases for fizzbuzz

--- solver.rb
+++ solver.rb
@@ -18,10 +18,13 @@ class Solver
     return solution.join
   end
   def fizzbuzz(n)
+    return 'fizzbuzz' if n % 15 == 0
     if n % 3 == 0
       'fizz'
     elsif n % 5 == 0
       'buzz'
+    else
+      n.to_s
     end
   end
 
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,21 +13,21 @@ describe Solver do
       result = @my_solver.fizzbuzz(20)
       expect(result).to eq('buzz')
     end
-    it "When N is divisible by 3 and 5, return 'fizzbuzz'" do
+    it "When N is divisible by 3 and 5 (15), return 'fizzbuzz'" do
       result = @my_solver.fizzbuzz(15)
       expect(result).to eq('fizzbuzz')
     end
-    it "When N is not divisible by 3 or 5, return N as a string" do
+    it "When N is 19, return N as a string" do
       result = @my_solver.fizzbuzz(19)
       expect(result).to eq('19')
     end
-    it "When N is not divisible by 3 or 5, return N as a string" do
+    it "When N is -7, return N as a string" do
       result = @my_solver.fizzbuzz(-7)
-      expect(result).to eq('fizzbuzz')
+      expect(result).to eq('-7')
     end
-    it "When N is not divisible by 3 or 5, return N as a string" do
+    it "When N is not divisible by 3 or 5 (3.5), return N as a string" do
       result = @my_solver.fizzbuzz(3.5)
-      expect(result).to eq('fizzbuzz')
+      expect(result).to eq('3.5')
     end
     it "fizzbuzz(0) should return 'fizzbuzz'" do
       result = @my_solver.fizzbuzz(0)
